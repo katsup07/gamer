41909b5b130418331218728f99e1b08c8133f0c7
katsup <brett_7y7@yahoo.ca>
2023-03-25 06:50:26 +0900

    Update delete

--- routes/games.js
+++ routes/games.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const Joi = require('joi');
-const {Game, createGame, getGames, getGame, updateGame, deleteGame } = require('../game-model');
+const {createGame, getGames, getGame, updateGame, deleteGame } = require('../game-model');
 
 // == /api/games ==
 router.get('/', async(req, res) => {
@@ -15,28 +16,37 @@ router.get('/', async(req, res) => {
 });
 
 router.get('/:id', async(req, res) => {
-  console.log(req.params);
   const { id } = req.params;
-  if(!id) return res.status(400).send('Game id is missing. Must be included to get game.');
-
+  if(!id) return res.status(400).send('Game id is missing.');
+  
+  if(!validateId(id)) return res.status(400).send('Invalid id');
+  
+  try{
   const game = await getGame(id);
   if(!game) return res.status(404).send('Game could not be found.');
 
   res.send(game);
+  } catch(err){
+    res.status(500).send('Something went wrong on the server');
+  }
 });
+
 
 router.post('/', async(req, res) => {
   const { body: game } = req;
-  console.log('req.body: ', game);
   const { error } = validateGame(game);
   if(error) return res.status(400).send(error.details[0].message);
-
+  
+  try{
   const games = await getGames();
   const gameExists = games.some( g => g.name === game.name);
   if(gameExists) return res.status(400).send('That game is already in database.')
 
-  const result = await createGame(game)
-  res.send(result);
+  const gameInDb = await createGame(game)
+  res.send(gameInDb);
+  } catch(err){
+    res.status(500).send('Something went wrong on the server');
+  }
 });
 
 router.put('/:id', async(req, res) => {
@@ -44,23 +54,39 @@ router.put('/:id', async(req, res) => {
   const { body: game } = req;
   const { error } = validateGame(game);
   if(error) return res.status(400).send(error.details[0].message);
-
+  
+  if(!validateId(id)) return res.status(400).send('Invalid id');
+  
+  try{
   const dbGame = await getGame(id);
   if(!dbGame) return res.status(400).send('No game with the given id was found.')
   // update game
   const updatedGame = await updateGame(id, game);
   res.send(updatedGame);
+  }catch(err){
+    res.status(500).send('Something went wrong on the server');
+  }
 });
 
-router.delete('/:id', (req, res) =>  {
+router.delete('/:id', async(req, res) =>  {
   const { id } = req.params;
-  const genre = genres.find( genre => genre.id === +id);
-  if(!genre) return res.status(404).send('No genre with the given id was found.');
+  try{
+  if(!validateId(id)) return res.status(400).send('Invalid id');
+  const game = await getGame(id);
+  if(!game) return res.status(404).send('No game with the given id was found.');
 
-  const index = genres.findIndex(genre => genre.id === id);
-  genres.splice(index, 1);
-  res.send(genre);
+
+  const result = await deleteGame(id); 
+  res.send(result);
+  }catch(err){
+    res.status(500).send('Something went wrong on the server');
+  }
 });
+
+// === helpers ===
+function validateId(id){
+  return mongoose.Types.ObjectId.isValid(id);
+}
 
 function validateGame(game){
   const schema = {
