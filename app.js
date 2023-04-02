de45b6f68335582b48bd43b1a1714b315db641cb
katsup <brett_7y7@yahoo.ca>
2023-04-02 21:42:26 +0900

    Export server

--- app.js
+++ app.js
@@ -15,4 +15,6 @@ startDB()
 setRoutes(app);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const server = app.listen(port, () => console.log(`Listening on port ${port}`));
+
+module.exports = server;
