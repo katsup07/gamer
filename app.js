e2c75c0a0be4e49e525940001e1df4aa103615b4
katsup <brett_7y7@yahoo.ca>
2023-04-21 20:27:11 +0900

    Refactor code

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ startDB()
 // routes
 setRoutes(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
 module.exports = server;
--- startup/routes.js
+++ startup/routes.js
@@ -5,16 +5,18 @@ const customerRoutes = require('../routes/customers');
 const orderRoutes = require('../routes/orders');
 const userRoutes = require('../routes/users');
 const authRoutes = require('../routes/auth');
+const cors = require('cors');
 
-module.exports = function(app){
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+module.exports = function (app) {
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: false }));
+	app.use(cors({ origin: '*' }));
 
-  app.use('/api/games', gameRoutes);
-  app.use('/api/customers', customerRoutes);
-  app.use('/api/orders', orderRoutes);
-  app.use('/api/users', userRoutes);
-  app.use('/api/auth', authRoutes);
+	app.use('/api/games', gameRoutes);
+	app.use('/api/customers', customerRoutes);
+	app.use('/api/orders', orderRoutes);
+	app.use('/api/users', userRoutes);
+	app.use('/api/auth', authRoutes);
 
-  app.use(errorHandler);
-}
+	app.use(errorHandler);
+};
